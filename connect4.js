89d5049000eac1f8d7160e25dedff75418c1c5d4
Nathan Newman <lordnate.moose@gmail.com>
2022-08-22 12:56:29 -0500

    updated connect4.js code

--- connect4.js
+++ connect4.js
@@ -4,40 +4,39 @@
  * column until a player gets four-in-a-row (horiz, vert, or diag) or until
  * board fills (tie)
  */
-
-const width = 7;
-const height = 6;
-
-let currPlayer = 1; // active player: 1 or 2
-const board = []; // array of rows, each row is array of cells  (board[y][x])
-
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
-
-function makeBoard() {
-  for (let y = 0; y < height; y++) {
-    const boardRow = [];
-    for (let x = 0; x < width; x++) {
-      boardRow.push(0);
-    }
-    board.push(boardRow);
+class ConnectFour {
+  constructor(height = 6, width = 7){
+    this.height = height; // height of the game board
+    this.width = width; // width of the game board
+    this.currPlayer = 1; // active player: 1 or 2
+    this.makeBoard(); // create in-JS board structure, board = array of rows, each row is array of cells  (board[y][x])
+    this.makeHtmlBoard(); // create HTML table and column tops that function as game board.
+    this.gameOver = false;
   }
-}
 
-/** makeHtmlBoard: make HTML table and row of column tops. */
+  function makeBoard() {
+  this.board = [];
+    for (let y = 0; y < this.height; y++) {
+      const boardRow = [];
+      for (let x = 0; x < this.width; x++) {
+      boardRow.push(0);
+      }
+    this.board.push(boardRow);
+    }
+  }
 
-function makeHtmlBoard() {
+  function makeHtmlBoard() {
   // variable that identifies the HTML table as the game board
   const htmlBoard = document.getElementById("board");
 
   // make clickable column row on top of the game board.
   const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
-  top.addEventListener("click", handleClick);
+  this.handleGameClick = this.handleClick.bind(this);
+  top.addEventListener("click", this.handleGameClick);
 
   // makes each cell for the column row.
-  for (let x = 0; x < width; x++) {
+  for (let x = 0; x < this.width; x++) {
     const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
@@ -46,9 +45,9 @@ function makeHtmlBoard() {
 
   // makes a table in HTML that functions as the game board board
   // each cell is given an id based on its x and y coordinate positioning.
-  for (let y = 0; y < height; y++) {
+  for (let y = 0; y < this.height; y++) {
     const row = document.createElement("tr");
-    for (let x = 0; x < width; x++) {
+    for (let x = 0; x < this.width; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
@@ -59,9 +58,9 @@ function makeHtmlBoard() {
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 // boolean that finds the first empty spot in the column.
-function findSpotForCol(x) {
-  for (let y = height - 1; y >= 0; y--) {
-    if (!board[y][x]) {
+  function findSpotForCol(x) {
+  for (let y = this.height - 1; y >= 0; y--) {
+    if (!this.board[y][x]) {
       return y;
     }
   }
@@ -70,10 +69,10 @@ function findSpotForCol(x) {
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
-function placeInTable(y, x) {
+  function placeInTable(y, x) {
   const piece = document.createElement("div");
   piece.classList.add("piece");
-  piece.classList.add(`p${currPlayer}`);
+  piece.classList.add(`p${this.currPlayer}`);
 
   const spot = document.getElementById(`${y}-${x}`);
   spot.append(piece);
@@ -81,38 +80,40 @@ function placeInTable(y, x) {
 
 /** endGame: announce game end */
 
-function endGame(msg) {
+  function endGame(msg) {
   alert(msg);
+  const top = document.querySelector("#column-top");
+  top.removeEventListener("click", this.handleGameClick);
 }
 
 /** handleClick: handle click of column top to play piece */
 
-function handleClick(evt) {
+  function handleClick(evt) {
   // get x from ID of clicked cell
   const x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
-  if (y === null) {
+  if (!y) {
     return;
   }
 
   // place piece in board and add to HTML table
-  board[y][x] = currPlayer;
+  this.board[y][x] = this.currPlayer;
   placeInTable(y, x);
 
   // check for win
   if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
+    return endGame(`Player ${this.currPlayer} won!`);
   }
 
   // check for tie
-  if (board.every((row) => row.every((cell) => cell))) {
+  if (this.board.every((row) => row.every((cell) => cell))) {
     return endGame("Tie!");
   }
 
   // switch players
-  currPlayer = currPlayer === 1 ? 2 : 1;
+  this.currPlayer = this.currPlayer === 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -126,17 +127,17 @@ function checkForWin() {
     return cells.every(
       ([y, x]) =>
         y >= 0 &&
-        y < height &&
+        y < this.height &&
         x >= 0 &&
-        x < width &&
-        board[y][x] === currPlayer
+        x < this.width &&
+        this.board[y][x] === this.currPlayer
     );
   }
 
   // The following code is for win conditions.
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
+  for (let y = 0; y < this.height; y++) {
+    for (let x = 0; x < this.width; x++) {
       // for a horizontal win, the y values will be the same for each piece.
       const horiz = [
         [y, x],
@@ -145,21 +146,21 @@ function checkForWin() {
         [y, x + 3],
       ];
       // for a vertical win, the x values will be the same for each piece.
-      let vert = [
+      const vert = [
         [y, x],
         [y + 1, x],
         [y + 2, x],
         [y + 3, x],
       ];
       // for a diagonal right win, the y and x values will be increment by 1 with each piece.
-      let diagDR = [
+      const diagDR = [
         [y, x],
         [y + 1, x + 1],
         [y + 2, x + 2],
         [y + 3, x + 3],
       ];
       // for a diagonal left win, as the y values increment for each piece, the x values will decrease.
-      let diagDL = [
+      const diagDL = [
         [y, x],
         [y + 1, x - 1],
         [y + 2, x - 2],
@@ -169,10 +170,12 @@ function checkForWin() {
       // returns true if a player meets any win condition.
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
+        }
       }
     }
   }
 }
+
 
 makeBoard();
 makeHtmlBoard();
